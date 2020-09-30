e7e137f9f4c8086f176373d150da79f3c41fe3c1
Maitreya1102 <67911242+Maitreya1102@users.noreply.github.com>
2020-09-30 17:00:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var database
+var canvas
+
+var gameState = 0;
+var playerCount = 0;
+
+function setup(){
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+
+}
+
+function draw(){
+  
+}
